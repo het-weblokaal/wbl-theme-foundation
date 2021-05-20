4b528a233bd3084de00090e4754d438cc73ddf18
Erik Joling <erik@hetweblokaal.nl>
2021-05-20 12:43:57 +0200

    Migrate responsibility of enqueueing to theme itself

--- theme/assets.php
+++ theme/assets.php
@@ -14,12 +14,6 @@ namespace WBL\Theme;
 
 add_action( 'after_setup_theme', function() {
 
-	// Manage default scripts & styles
-	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\add_frontend_scripts_and_styles' );
-
-	// Add theme editor style to editor
-	add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\add_editor_scripts_and_styles' );
-
 	// Remove google font from block editor
 	add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\remove_google_font_from_block_editor' );
 
@@ -28,55 +22,6 @@ add_action( 'after_setup_theme', function() {
 # ------------------------------------------------------------------------------
 # Functions
 # ------------------------------------------------------------------------------
-
-/**
- * Add default scripts/styles
- */
-function add_frontend_scripts_and_styles() {
-
-	// Add default script js/theme.js
-	wp_enqueue_script( 
-		Theme::handle(), 
-		Theme::asset( 'js/theme.js' ), 
-		null, 
-		null, 
-		true 
-	);
-
-	// Add default stylesheet css/styles.css
-	wp_enqueue_style( Theme::handle(), Theme::asset( 'css/style.css' ), null, null );
-}
-
-/**
- * Remove block-editor styles
- */
-function remove_frontend_block_editor_styles() {
-	wp_dequeue_style( 'wp-block-library' );
-}
-
-
-/**
- * Remove core editor google font
- */
-function add_editor_scripts_and_styles() {
-
-	// Add script for block editor
-	wp_enqueue_script( 
-		Theme::handle('block-editor'), 
-		Theme::asset( 'js/block-editor.js' ), 
-		[ 
-			'wp-blocks', 
-			'wp-i18n', 
-			'wp-data', 
-			'wp-dom-ready', 
-			'wp-edit-post', 
-			'wp-hooks'
-		] 
-	);
-
-	// Add styles for block editor
-	wp_enqueue_style( Theme::handle('editor'), Theme::asset( 'css/editor-style.css' ), null, null );
-}
 
 
 /**
